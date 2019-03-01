e0d01a73162f8da3ffb39eb013d14be5322d8490
Patrick Hannon <patrick_hannon@hotmail.com>
2019-03-01 17:29:44 -0600

    initial commit

---
+++ LdapHelper.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.DirectoryServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.DirectoryServices.Protocols;
+using System.Net;
+using System.Security.Cryptography;
+using Hannon.Ldap.Models;
+using SearchScope = System.DirectoryServices.Protocols.SearchScope;
+
+namespace Hannon.Ldap
+{
+    public class LdapHelper
+    {
+        private LdapConnection _connection;
+        public LdapHelper(string username, string domain, string password, string url)
+        {
+            var credentials = new NetworkCredential(username, password, domain);
+            var serverId = new LdapDirectoryIdentifier(url);
+
+            _connection = new LdapConnection(serverId, credentials);
+
+        }
+        public bool ValidateUserByBind(string username, string password)
+        {
+            bool result = true;
+            var credentials = new NetworkCredential(username, password);
+            var serverId = new LdapDirectoryIdentifier(_connection.SessionOptions.HostName);
+
+            var conn = new LdapConnection(serverId, credentials);
+            try
+            {
+                conn.Bind();
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+            conn.Dispose();
+            return result;
+        }
+        public bool ValidateUser(string username, string password)
+        {
+            var sha1 = new SHA1Managed();
+            var digest = Convert.ToBase64String(sha1.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password)));
+            var request = new CompareRequest(string.Format("uid={0},ou=users,dc=example,dc=com", username),
+                "userPassword", "{SHA}" + digest);
+            var response = (CompareResponse)_connection.SendRequest(request);
+            return response.ResultCode == ResultCode.CompareTrue;
+        }
+        public void AddUser(UserModel user)
+        {
+            DirectoryEntry ouEntry = new DirectoryEntry("LDAP://CN=Users,DC=Hannon,DC=local");
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    DirectoryEntry childEntry = ouEntry.Children.Add("CN=TestUser" + i, "user");
+                    childEntry.CommitChanges();
+                    ouEntry.CommitChanges();
+                    childEntry.Invoke("SetPassword", new object[] { "Password123" });
+                    childEntry.CommitChanges();
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex);
+                }
+            }
+        }
+
+        public List<Dictionary<string, string>> search(string baseDn, string ldapFilter)
+        {
+            var request = new SearchRequest(baseDn, ldapFilter, SearchScope.Subtree, null);
+            var response = (SearchResponse)_connection.SendRequest(request);
+
+            var result = new List<Dictionary<string, string>>();
+
+            foreach (SearchResultEntry entry in response.Entries)
+            {
+                var dic = new Dictionary<string, string>();
+                dic["DN"] = entry.DistinguishedName;
+
+                foreach (string attrName in entry.Attributes.AttributeNames)
+                {
+                    //For simplicity, we ignore multi-value attributes
+                    dic[attrName] = string.Join(",", entry.Attributes[attrName].GetValues(typeof(string)));
+                }
+
+                result.Add(dic);
+            }
+
+            return result;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Hannon.Ldap.Models;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace Hannon.Ldap
+{
+    class Program
+    {
+        private static string _lDAPUser;
+        private static string _lDAPPath;
+        private static string _lDAPPassword;
+        private static string _lDAPDomain;
+
+        static void Main(string[] args)
+        {
+            _lDAPUser = ConfigurationManager.AppSettings["LDAPUser"];
+            _lDAPDomain = ConfigurationManager.AppSettings["LDAPPath"];
+            _lDAPPassword = ConfigurationManager.AppSettings["LDAPPassword"];
+            _lDAPPath = ConfigurationManager.AppSettings["LDAPDomain"];
+
+            //if (args.Length != 4)
+            //{
+            //    Debug.WriteLine("Usage: LDAPClient <username> <domain> <password> <LDAP server URL>");
+            //    Environment.Exit(1);
+            //}
+            //var client = new LdapHelper(args[0], args[1], args[2], args[3]);
+
+            var client = new LdapHelper(_lDAPUser, _lDAPDomain, _lDAPPassword, _lDAPPath);
+
+            //Searching for all users
+            var searchResult = client.search("cn=Users,dc=hannon,dc=local", "objectClass=*");
+            foreach (Dictionary<string, string> d in searchResult)
+            {
+                Debug.WriteLine(String.Join("\r\n", d.Select(x => x.Key + ": " + x.Value).ToArray()));
+            }
+
+            //Validating credentials using LDAP bind
+            //For this to work the server must be configured to map users correctly to its internal database
+            if (client.ValidateUserByBind(_lDAPUser, _lDAPPassword))
+            {
+                Debug.WriteLine("Valid credentials (bind)");
+            }
+            else
+            {
+                Debug.WriteLine("Invalid credentials (bind)");
+            }
+
+            try
+            {
+                //Adding a user
+                client.AddUser(new UserModel("cn=sampleuser,cn=users,dc=hannon,dc=local",
+                    "sampleuser", "Users", "plaintextpass"));
+            }
+            catch (Exception e)
+            {
+                //The user may already exist
+                Debug.WriteLine(e);
+            }
+
+        }
+    }
+}
